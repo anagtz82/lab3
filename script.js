6138d20eec0a857d4655250385287c16cbd84fcd
anagtz82 <anagtz82@me.com>
2018-01-18 12:41:50 -0600

    update on upper case name

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 var friends = ["Cam", "Bo", "Kerryon", "Deshaun", "Tristan"];
+console.log(`${friends}length.toUpperCase`);
 
-for (var i = 0; i < friends.length; i++) {
-    console.log(friends[i]);
+
+for (var f = 0; f < friends.length; f++) {
+    for (var i = 99; i > 0; i--) {
+        if (i === 2) {
+            console.log(`${i} lines of code in the file, ${i}  lines of code; ${friends[f]} strikes one out, clears it all out, ${i-1}  line of code in the file`);
+        } else if (i === 1) {
+            console.log(`${i} lines of code in the file, ${i}  lines of code; ${friends[f]} strikes one out, clears it all out, no more lines of code in the file`);
+        } else {
+            console.log(`${i} lines of code in the file, ${i}  lines of code; ${friends[f]} strikes one out, clears it all out, ${i-1}  lines of code in the file`);
+
+        }
+    }
 }
